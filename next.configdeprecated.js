66c4b77a556938520d9455e66e5d64233a064215
saba <sbsilagadze@gmail.com>
2023-01-16 00:10:01 +0400

    trying to setup sentry"

---
+++ next.configdeprecated.js
@@ -0,0 +1,14 @@
+module.exports = {
+  reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  i18n: {
+    locales: ['ge'],
+    defaultLocale: 'ge',
+  },
+}
