3cc408ddd534b5d05bf78c4df514fb93443429e3
saba <sbsilagadze@gmail.com>
2023-02-26 15:21:51 +0400

    improved filtration panel

--- components/filtration.tsx
+++ components/filtration.tsx
@@ -3,13 +3,14 @@ import ScrollContainer from 'react-indiana-drag-scroll'
 import { Dispatch, SetStateAction, useCallback, useRef, useState } from "react";
 import axios from "axios";
 import fetchVanacies from "database/vacancies/fetchVacanciesByCompanyId";
-import { FiltrationItem } from "./buttons/filtration/filtrationItem";
+// import { FiltrationItem } from "./buttons/filtration/filtrationItem";
 import { FilterTypes, ToggledFilters, Vacancy, VacancyWithCompany } from "types";
 import fetchVacanciesByTechnologies from "database/vacancies/fetchVacanciesByTechnologies";
 import TechnologyInput from "./inputs/technologyInput";
 import removeDuplicatesFromArray from "functions/utils/remoteDuplicatesFromArray";
+import { AiOutlineCloseCircle } from 'react-icons/ai';
 
-let technologies = ["Typescript", "PHP", "Java", "Angular", "React", "Vue", "C#", ".Net", "Go", "Rust", "MongoDB", "MySQL", "PostgreSQL", "react1", "react2", "react3", "react4", "react5", "react6"];
+let technologies = ["Typescript", "PHP", "Java", "Angular", "React", "Vue", "C#", ".Net", "Go", "Rust", "MongoDB", "MySQL", "PostgreSQL", "Firebase", "Flask"];
 const seniority = ["Intern", "Junior", "Middle", "Senior", "Lead"];
 const companies = ["BOG", "TBC", "Flat Rock Technology", "Flat Rock Technology", "Flat Rock Technology", "Flat Rock Technology"];
 
@@ -45,12 +46,20 @@ export default function Filtration(args: FiltrationArgs) {
         }
     }
 
+    function clearAllTechnologies() {
+        toggledFilters.current.technologies = [];
+
+        // TODO : Implement search with seniorities
+
+        setSalaries([]);
+    }
+
     let filteredTechnologies = technologies.filter(tech => tech.toLocaleLowerCase().includes(input.toLocaleLowerCase()))
     
-    // filteredTechnologies = [...toggledFilters.current.technologies, ...filteredTechnologies];
     filteredTechnologies = removeDuplicatesFromArray(toggledFilters.current.technologies, filteredTechnologies);
     
     console.log("filteredTechnologies", filteredTechnologies);
+    console.log("techs", toggledFilters.current.technologies);
 
     return (
         <div className={style.filtration}>
@@ -58,10 +67,19 @@ export default function Filtration(args: FiltrationArgs) {
                 {/* <TechnologyInput /> */}
                 <input type="text" placeholder="ტექნოლოგია" value={input} onChange={e => setInput(e.target.value)} />
 
-                <DisplayButtons elements={filteredTechnologies} toggleFilter={ToggleFilter("technologies")} />
+                { toggledFilters.current.technologies.length > 0 &&
+                    <AiOutlineCloseCircle
+                        onClick={clearAllTechnologies} 
+                        color="rgb(0, 21, 212, .85)"
+                        size={"2rem"} 
+                        style={{ margin: "0 .5rem 0 0", cursor: "pointer" }}
+                    />
+                }
+
+                <DisplayButtons elements={filteredTechnologies} activeElements={toggledFilters.current.technologies} toggleFilter={ToggleFilter("technologies")} />
             </div>
             
-            <DisplayButtons elements={seniority} toggleFilter={ToggleFilter("seniorities")} />
+            <DisplayButtons elements={seniority} activeElements={toggledFilters.current.seniorities} toggleFilter={ToggleFilter("seniorities")} />
 
             {/* <SubmitFiltration setVacancies={setSalaries} toggledFilters={toggledFilters.current} /> */}
         </div>
@@ -71,15 +89,40 @@ export default function Filtration(args: FiltrationArgs) {
 
 interface DisplayButtonsArgs {
     elements: string[],
+    activeElements: string[],
     toggleFilter: (filter: string, action: "add" | "remove") => void
 }
 
 function DisplayButtons(args: DisplayButtonsArgs) {
-    const { elements, toggleFilter } = args;
+    const { elements, toggleFilter, activeElements } = args;
 
     return (
         <ScrollContainer className={style.filtrationRow}>
-            { elements.map((element, index) => <FiltrationItem text={element} key={element} toggleFilter={toggleFilter} />) }
+            { elements.map((element, index) => <FiltrationItem text={element} key={element} isActive={activeElements.includes(element)} toggleFilter={toggleFilter} />) }
         </ScrollContainer>
+    )
+}
+
+
+interface PillButtonArgs {
+    text: string
+    isActive?: boolean
+    toggleFilter: (filter: string, action: "add" | "remove") => void
+}
+
+export function FiltrationItem(args: PillButtonArgs) {
+    // const [isActive, setActive] = useState(false);
+    const { text, toggleFilter, isActive } = args;
+
+    function handleClick() {
+        toggleFilter(text, isActive ? "remove" : "add");
+
+        // setActive(active => !active)
+    }
+
+    return (
+        <button onClick={handleClick} className={`${style.filtrationItem} ${!isActive || style.active}`}>
+            { text }
+        </button>
     )
 }
